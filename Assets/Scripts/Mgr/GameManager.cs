7b32cc40634bccf300f15bd3d80f0041da9bda9a
H-Jungjaeheon <jjhking222@naver.com>
2022-04-12 01:11:46 +0900

    GM
    GM1

--- Assets/Scripts/Mgr/GameManager.cs
+++ Assets/Scripts/Mgr/GameManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class GameManager : SingletonMono<GameManager>
 {
+    public float PlayerHp, EnemyHp;
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.In.PlayerHp = 0;  
+        
     }
 
     // Update is called once per frame
