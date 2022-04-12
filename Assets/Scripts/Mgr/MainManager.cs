bb43ac5cfcf56f1e1f46024777ce93e0c0d3fbf8
sungbum05 <sblim05@naver.com>
2022-04-12 21:56:41 +0900

    DestroyTrash

---
+++ Assets/Scripts/Mgr/MainManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    [Header("Gizmo - 소환 영역 표시")]
+    [SerializeField]
+    Vector2 SpawnPos;
+
+    [SerializeField]
+    float SizeX;
+    [SerializeField]
+    float SizeY;
+
+    [Header("실제영역계산(실제스폰영역 확인)")] // 에디터에서 건드리지 않아도 됌
+    [SerializeField]
+    GameObject Boundary;
+
+    [SerializeField]
+    float SpawnPosX, SpawnPosY;
+
+    [Header("소환 변수")]
+    [SerializeField]
+    GameObject Trash;
+    [SerializeField]
+    float SpawnWaitTime;
+    [SerializeField] // 레이어 마스크(체크용)
+    int layerMask  = 1 << 6;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTrash());
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MouseClick();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(SpawnPos, new Vector2(SizeX, SizeY));
+
+        Boundary.transform.position = SpawnPos;
+        Boundary.transform.localScale = new Vector2(SizeX, SizeY);
+
+        SpawnPosX = Boundary.gameObject.GetComponent<BoxCollider2D>().bounds.extents.x;
+        SpawnPosY = Boundary.gameObject.GetComponent<BoxCollider2D>().bounds.extents.y;
+    }
+
+    private void MouseClick()
+    {
+        Vector2 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Ray2D ray = new Ray2D(Pos, Vector2.zero);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 1.0f, layerMask);
+
+        if(hit.collider != null)
+        {
+            hit.collider.gameObject.GetComponent<Main_Trash>().OnHit();
+        }
+    }
+
+    IEnumerator SpawnTrash()
+    {
+        yield return null;
+
+        float RanPosX = Random.Range(-SpawnPosX, SpawnPosX + 0.1f);
+        float RanPosY = Random.Range(-SpawnPosY, SpawnPosY + 0.1f);
+
+        GameObject TrashObj = Instantiate(Trash, new Vector2(RanPosX, RanPosY), Quaternion.identity);
+
+        yield return new WaitForSeconds(SpawnWaitTime + Random.Range(-0.5f, 0.6f));
+        StartCoroutine(SpawnTrash());
+    }
+}
