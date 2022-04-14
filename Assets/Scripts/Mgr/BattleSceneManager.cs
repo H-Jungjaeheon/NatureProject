06c1f4fb4a40d13699a8fe7b6347fc40d12fb120
H-Jungjaeheon <jjhking222@naver.com>
2022-04-15 01:37:44 +0900

    StopUI
    StopUI

--- Assets/Scripts/Mgr/BattleSceneManager.cs
+++ Assets/Scripts/Mgr/BattleSceneManager.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BattleSceneManager : SingletonMono<BattleSceneManager>
 {
-    public bool IsPass; //소환 버튼 넘김
+    [Header("전투씬 관련 변수들")]
+    public bool IsPass, IsStop; //소환 버튼 넘김
     public float PlayerHp, MaxPlayerHp, EnemyHp, MaxEnemyHp, Money, MaxMoney, FireCoolTime,
     MaxFireCoolTime, MoneyLevel, MaxMoneyLevel, UpgradeNeedMoney, Timer;
+    [Header("전투씬 시간 초 변수")]
     public int Min = 0, ST = 0;
+    [Header("전투씬 텍스트 UI")]
     public Text MoneyText, MoneyLevelText, UpgradeNeedMoneyText, StageText, TimeText;
+    [Header("전투씬 이미지 UI")]
     public Image PlayerHpBar, EnemyHpBar, FireCoolTimeImage;
+    [Header("전투씬 커서(터치) 변수")]
     [SerializeField] private Vector2 Pos, Pos2;
     [SerializeField] private bool IsTouch;
+    [Header("일시 정지 오브젝트")]
+    [SerializeField] private GameObject StopObj;
+
     private void Start()
     {
         Timer = 0;
@@ -35,59 +44,68 @@ public class BattleSceneManager : SingletonMono<BattleSceneManager>
     }
     void BattleAmounts()
     {
-        Money += Time.deltaTime * (2 + MoneyLevel); //나중에 10에다가 돈 업그레이드 레벨 or 레벨당 수치만큼 더해주기
-        Timer += Time.deltaTime;
-        FireCoolTime -= Time.deltaTime * (5); //나중에 5에다가 쿨타임 업그레이드 레벨 or 레벨당 수치만큼 더해주기
-        if(Money >= MaxMoney)       
-            Money = MaxMoney;
-        if(FireCoolTime <= 0)
-            FireCoolTime = 0;
+        if (IsStop == false)
+        {
+            Money += Time.deltaTime * (2 + MoneyLevel); //나중에 10에다가 돈 업그레이드 레벨 or 레벨당 수치만큼 더해주기
+            Timer += Time.deltaTime;
+            FireCoolTime -= Time.deltaTime * (5); //나중에 5에다가 쿨타임 업그레이드 레벨 or 레벨당 수치만큼 더해주기
+            if (Money >= MaxMoney)
+                Money = MaxMoney;
+            if (FireCoolTime <= 0)
+                FireCoolTime = 0;
+        }
     }
     void BattleUI()
     {
-        if (Timer > 1)
+        if (IsStop == false)
         {
-            Timer = 0;
-            ST++;
+            if (Timer > 1)
+            {
+                Timer = 0;
+                ST++;
+            }
+            if (ST > 59)
+            {
+                Min++;
+                ST = 0;
+            }
+            MoneyText.text = $"{Money:N0} / {MaxMoney} 원";
+            TimeText.text = string.Format($"{Min:D2} : {ST:D2}");
+            //StageText.text = ""; 나중에 스테이지 정보마다 바꾸기
+            MoneyLevelText.text = $"Level.{MoneyLevel:N0}";
+            PlayerHpBar.fillAmount = PlayerHp / MaxPlayerHp;
+            EnemyHpBar.fillAmount = EnemyHp / MaxEnemyHp;
+            FireCoolTimeImage.fillAmount = FireCoolTime / MaxFireCoolTime;
+            if (MoneyLevel < MaxMoneyLevel)
+                UpgradeNeedMoneyText.text = $"{UpgradeNeedMoney:N0} 원";
+            else
+                UpgradeNeedMoneyText.text = "Max";
+            if (PlayerHp >= MaxPlayerHp)
+                PlayerHp = MaxPlayerHp;
+            if (EnemyHp >= MaxEnemyHp)
+                EnemyHp = MaxEnemyHp;
+            if (FireCoolTime <= 0)
+                FireCoolTime = 0;
         }
-        if(ST > 59)
-        {
-            Min++;
-            ST = 0;
-        }
-        MoneyText.text = $"{Money:N0} / {MaxMoney} 원";
-        TimeText.text = string.Format($"{Min:D2} : {ST:D2}");
-        //StageText.text = ""; 나중에 스테이지 정보마다 바꾸기
-        MoneyLevelText.text = $"Level.{MoneyLevel:N0}";
-        PlayerHpBar.fillAmount = PlayerHp / MaxPlayerHp;
-        EnemyHpBar.fillAmount = EnemyHp / MaxEnemyHp;
-        FireCoolTimeImage.fillAmount = FireCoolTime / MaxFireCoolTime;
-        if (MoneyLevel < MaxMoneyLevel)
-            UpgradeNeedMoneyText.text = $"{UpgradeNeedMoney:N0} 원";
-        else
-            UpgradeNeedMoneyText.text = "Max";
-        if (PlayerHp >= MaxPlayerHp)
-            PlayerHp = MaxPlayerHp;
-        if (EnemyHp >= MaxEnemyHp)
-            EnemyHp = MaxEnemyHp;
-        if (FireCoolTime <= 0)
-            FireCoolTime = 0;
     }
     void DragInput()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (IsStop == false)
         {
-            Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            IsTouch = true;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            IsTouch = false;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                IsTouch = true;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                IsTouch = false;
+            }
         }
     }
     void SpawnButtonMove()
     {
-        if (IsTouch == true)
+        if (IsTouch == true && IsStop == false)
         {
             Pos2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if (Pos2.y < Pos.y - 1 && Pos.x + 0.6f >= Pos2.x && Pos.x - 0.6f <= Pos2.x)
@@ -102,7 +120,7 @@ public class BattleSceneManager : SingletonMono<BattleSceneManager>
     }
     public void UpGradeMoney()
     {
-        if(MoneyLevel < MaxMoneyLevel && Money >= UpgradeNeedMoney)
+        if (MoneyLevel < MaxMoneyLevel && Money >= UpgradeNeedMoney && IsStop == false)
         {
             Money -= UpgradeNeedMoney;
             MoneyLevel++;
@@ -112,7 +130,7 @@ public class BattleSceneManager : SingletonMono<BattleSceneManager>
     }
     public void Fire()
     {
-        if(FireCoolTime <= 0)
+        if(FireCoolTime <= 0 && IsStop == false)
         {
             FireCoolTime = MaxFireCoolTime;
             //대포 발사 능력 작동
@@ -120,7 +138,18 @@ public class BattleSceneManager : SingletonMono<BattleSceneManager>
     }
     public void GameStopButton()
     {
-        Time.timeScale = 0;
-        //멈춤 창 띄우기
+        if(IsStop == false)
+        {
+            IsStop = true;
+            StopObj.transform.DOScale(1, 1.5f).SetEase(Ease.OutBack);
+        }
+    }
+    public void ExitButton()
+    {
+        if(IsStop== true)
+        {
+            StopObj.transform.DOScale(0, 0.8f).SetEase(Ease.InBack);
+            IsStop = false;
+        }
     }
 }
