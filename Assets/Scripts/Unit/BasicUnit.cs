3a8a9ebe3f0f670c4b488c4290665c3988f2d265
H-Jungjaeheon <jjhking222@naver.com>
2022-04-20 21:24:21 +0900

    BasicUnit
    BasicUnit

--- Assets/Scripts/Unit/BasicUnit.cs
+++ Assets/Scripts/Unit/BasicUnit.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class BasicUnit : MonoBehaviour
 {
-    [SerializeField] private float Hp, MaxHp, Damage, Speed, Range;
+    [SerializeField] private float Hp, MaxHp, Damage, ReceivDamage, Speed, Range, KnockBackCount;
     [SerializeField] private bool IsKnockBack;
     [SerializeField] private GameObject Target;
 
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         Move();
+    }
+    private void FixedUpdate()
+    {
         Attack();
+        StatManagement();
+        KnockBack();
+    }
+    public virtual void KnockBack()
+    {
+        //넉백 구현
+        if(ReceivDamage == (MaxHp / KnockBackCount))
+        {
+            ReceivDamage = 0;
+        }
+        else if(ReceivDamage > (MaxHp / KnockBackCount))
+        {
+            ReceivDamage = ReceivDamage - (MaxHp / KnockBackCount);
+        }
+        //체력이 0 이거나 0이하면, 넉백 실행 후 사망처리
         Dead();
+    }
+    public virtual void StatManagement()
+    {
+        if (Hp >= MaxHp)
+            Hp = MaxHp;
     }
     public virtual void Move()
     {
@@ -29,13 +52,18 @@ public class BasicUnit : MonoBehaviour
     {
         Debug.DrawRay(transform.position, new Vector3(Range, 0, 0), Color.red);
         RaycastHit2D RH = Physics2D.Raycast(transform.position, new Vector3(Range, 0, 0));
-        if (RH.collider.CompareTag("Enemy"))
-        {
-             
-        }
+        //if (RH.collider.CompareTag("Enemy") && RH.collider.gameObject != false)
+        //{
+        //    Target = RH.collider.gameObject;
+        //}
+        //움직임 멈추고 공격 시작
     }
     public virtual void Dead()
     {
-
+        if(Hp <= 0)
+        {
+            Destroy(this.gameObject);
+            //죽음 효과 소환
+        }
     }
 }
