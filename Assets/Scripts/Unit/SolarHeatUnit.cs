2a0bc4e83333e26e53a618002c971727173e27d7
H-Jungjaeheon <jjhking222@naver.com>
2022-05-12 20:40:16 +0900

    BasicUnitMake
    BasicUnitMake

---
+++ Assets/Scripts/Unit/SolarHeatUnit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarHeatUnit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //화면 스크롤 적 성 만들기
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
