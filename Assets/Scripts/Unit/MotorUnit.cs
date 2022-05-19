06853ac5b715730fe7a2dfa7c8d376e4eb4fad89
H-Jungjaeheon <jjhking222@naver.com>
2022-05-19 20:49:50 +0900

    Motor
    Motor

--- Assets/Scripts/Unit/MotorUnit.cs
+++ Assets/Scripts/Unit/MotorUnit.cs
@@ -4,63 +4,67 @@ using UnityEngine;
 
 public class MotorUnit : BasicUnit
 {
-    private RaycastHit2D[] Hit;
     protected override void AttackCoolTime()
     {
-        AttackCoolTimeCount = (IsAttackSlow == true) ? AttackCoolTimeCount += Time.deltaTime / 1.5f : AttackCoolTimeCount += Time.deltaTime;
+        //AttackCoolTimeCount = (IsAttackSlow == true) ? AttackCoolTimeCount += Time.deltaTime / 1.5f : AttackCoolTimeCount += Time.deltaTime;
         Debug.DrawRay(transform.position, Vector2.right * Range, Color.red);
-        Hit = Physics2D.RaycastAll(transform.position, Vector2.right, Range, LayerMask.GetMask("Enemy"));
+        RaycastHit2D[] Hit = Physics2D.RaycastAll(transform.position, Vector2.right, Range, LayerMask.GetMask("Enemy"));
         RaycastHit2D castlehit = Physics2D.Raycast(transform.position, Vector2.right, Range, LayerMask.GetMask("EnemyCastle"));
 
         if (castlehit.collider != null) ECTarget = castlehit.collider.gameObject;
         else ECTarget = null;
 
-        if (ECTarget != null || Hit != null)
+        if (ECTarget != null || Hit.Length > 0 && Hit[0].collider != null)
         {
-            for (int a = 0; a < Hit.Length; a++)
+            IsAttackReady = true;
+            if (AttackCoolTimeCount >= MaxAttackCoolTimeCount && IsAttackReady == true)
             {
-                RaycastHit2D NowHits = Hit[a];
-                if (NowHits && NowHits.collider != null)
+                AttackAnim();
+                AttackCount = (IsAttackSlow == true) ? AttackCount += Time.deltaTime / 1.5f : AttackCount += Time.deltaTime;
+                if (AttackCount >= MaxAttackCount)
                 {
-                    Target = NowHits.collider.gameObject;
-                }
-                else
-                {
-                    Target = null;
-                }
-
-                if (Hit[a].collider.GetComponent<BasicEnemy>().IsKnockBack == false || ECTarget != null)
-                {
-                    NowHits = new RaycastHit2D();
-                    IsAttackReady = true;
-                    if (AttackCoolTimeCount >= MaxAttackCoolTimeCount && IsAttackReady == true)
+                    for (int b = 0; b < Hit.Length; b++)
                     {
-                        AttackAnim();
-                        AttackCount = (IsAttackSlow == true) ? AttackCount += Time.deltaTime / 1.5f : AttackCount += Time.deltaTime;
-                        if (AttackCount >= MaxAttackCount && Target && Target.GetComponent<BasicEnemy>().IsKnockBack == false)
+                        Debug.Log(Target);
+                        if (Hit[b] && Hit[b].collider != null)
+                        {
+                            Target = Hit[b].collider.gameObject;
+                        }
+                        else
+                        {
+                            Target = null;
+                        }
+
+                        if (Target  && Target.GetComponent<BasicEnemy>().IsKnockBack == false || ECTarget != null)
                         {
                             Target.GetComponent<BasicEnemy>().Hp -= Damage;
                             Target.GetComponent<BasicEnemy>().ReceivDamage += Damage;
                             Target = null;
-                            if (a == Hit.Length - 1)
+                            if (b == Hit.Length - 1)
                             {
-                                Debug.Log(a);
-                                AttackCount = 0;
+                                Debug.Log(Hit.Length);
+                                if (ECTarget)
+                                {
+                                    BGameManager.GetComponent<BattleSceneManager>().EnemyHp -= Damage;
+                                    ECTarget.GetComponent<EnemyCastle>().IsHit = true;
+                                    ECTarget = null;
+                                    Hit = null;
+                                }
+                                Hit = null;
                             }
                         }
                     }
-                    else if (AttackCoolTimeCount < MaxAttackCoolTimeCount && IsAttackReady == true)
-                    {
-                        //기본 애니 실행
-                    }
+                    AttackCount = 0;
                 }
             }
-            if (ECTarget)
+            else if (AttackCoolTimeCount < MaxAttackCoolTimeCount && IsAttackReady == true)
             {
-                BGameManager.GetComponent<BattleSceneManager>().EnemyHp -= Damage;
-                ECTarget.GetComponent<EnemyCastle>().IsHit = true;
-                ECTarget = null;
+                //기본 애니 실행
             }
+        }
+        else
+        {
+            IsAttackReady = false;
         }
     }
 }
