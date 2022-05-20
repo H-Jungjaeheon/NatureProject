c41de22653db5fc30c3e4436245822b74ca615df
sungbum05 <sblim05@naver.com>
2022-05-20 12:00:33 +0900

    Forming작업중

--- Assets/Scripts/Mgr/FormingManager.cs
+++ Assets/Scripts/Mgr/FormingManager.cs
@@ -28,8 +28,21 @@ public class FormingManager : MonoBehaviour
         foreach (GameUnitData Data in GameManager.In.GameUnitData)
         {
             GameObject FormingType = Instantiate(FormingBoxImage);
+            FormingType.name = $"{Data.UnitName.ToString()}_FormingBox";
             FormingType.transform.parent = Contents.transform;
             FormingType.transform.localScale = Vector3.one;
+            FormingType.transform.GetChild(0).gameObject.GetComponent<RectTransform>().sizeDelta 
+                = new Vector2(Data.UnitImage.textureRect.width, Data.UnitImage.textureRect.height);
+            FormingType.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = Data.UnitImage;
+            Debug.Log($"{Data.UnitImage.name}: ({Data.UnitImage.texture.width},{Data.UnitImage.texture.height})");
+
+            //if (Data.UnitLevel > 0)
+            //{
+            //    GameObject FormingType = Instantiate(FormingBoxImage);
+            //    FormingType.name = $"{Data.UnitName.ToString()}_FormingBox";
+            //    FormingType.transform.parent = Contents.transform;
+            //    FormingType.transform.localScale = Vector3.one;
+            //}
         }
     }
 
