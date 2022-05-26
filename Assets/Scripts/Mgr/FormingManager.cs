3162059b6dc8f66bbb99c634df2478cf0ac524f8
sungbum05 <sblim05@naver.com>
2022-05-26 10:37:24 +0900

    FormingImgSize

--- Assets/Scripts/Mgr/FormingManager.cs
+++ Assets/Scripts/Mgr/FormingManager.cs
@@ -25,15 +25,23 @@ public class FormingManager : MonoBehaviour
 
     void SceneSetting()
     {
+        GameObject FormingCharacter = null;
+
         foreach (GameUnitData Data in GameManager.In.GameUnitData)
         {
             GameObject FormingType = Instantiate(FormingBoxImage);
             FormingType.name = $"{Data.UnitName.ToString()}_FormingBox";
             FormingType.transform.parent = Contents.transform;
             FormingType.transform.localScale = Vector3.one;
-            FormingType.transform.GetChild(0).gameObject.GetComponent<RectTransform>().sizeDelta 
+
+            FormingCharacter = FormingType.transform.GetChild(0).transform.GetChild(0).gameObject;
+            FormingCharacter.gameObject.GetComponent<RectTransform>().sizeDelta 
                 = new Vector2(Data.UnitImage.textureRect.width, Data.UnitImage.textureRect.height);
-            FormingType.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = Data.UnitImage;
+
+            FormingCharacter.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(Data.FormingPosX, Data.FormingPosY, 0);
+
+            FormingCharacter.gameObject.GetComponent<Image>().sprite = Data.UnitImage;
+
             Debug.Log($"{Data.UnitImage.name}: ({Data.UnitImage.texture.width},{Data.UnitImage.texture.height})");
 
             //if (Data.UnitLevel > 0)
--- Assets/Scripts/Unit/GameUnitData.cs
+++ Assets/Scripts/Unit/GameUnitData.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 [System.Serializable]
 public class GameUnitData
 {
+    public enum CharacterClass
+    {
+        Common,
+        Rare,
+        Unique,
+        Legendary,
+    }
+
+    [Header("필수 정보")]
     public int UnitID;
     public string UnitName;
-    public string UnitClass;
+    public CharacterClass UnitClass;
     public Sprite UnitImage;
     public int UnitLevel;
     public int UnitCost;
     public int UnitPower;
     public int UnitHP;
+
+    [Header("선택 정보")]
+    public float FormingPosX;
+    public float FormingPosY;
 }
